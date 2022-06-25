bf73a41d21d3879d6eed63a737486a4cbc389aa3
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-25 22:20:11 +0530

    Update zip.py

--- AyraRobot/modules/zip.py
+++ AyraRobot/modules/zip.py
@@ -5,9 +5,9 @@ import zipfile
 from telethon import types
 from telethon.tl import functions
 
-from FallenRobot import TEMP_DOWNLOAD_DIRECTORY
-from FallenRobot import telethn as client
-from FallenRobot.events import register
+from AyraRobot import TEMP_DOWNLOAD_DIRECTORY
+from AyraRobot import telethn as client
+from AyraRobot.events import register
 
 
 async def is_register_admin(chat, user):
