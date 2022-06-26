c632dab2753ed3e9519dd5bef95965a35ec62292
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 21:51:36 +0530

    Update approve_sql.py

--- AyraRobot/modules/sql/approve_sql.py
+++ AyraRobot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, BigInteger, func, distinct
 
-from FallenRobot.modules.sql import BASE, SESSION
+from AyraRobot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
