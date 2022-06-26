693f42f97cf9df442ff3e853fc58bc83f195deaa
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 20:52:41 +0530

    Update carbon.py

--- AyraRobot/modules/carbon.py
+++ AyraRobot/modules/carbon.py
@@ -1,8 +1,8 @@
 from pyrogram.types import Message
 from pyrogram import filters
-from FallenRobot import pbot
-from FallenRobot.utils.errors import capture_err
-from FallenRobot.utils.functions import make_carbon
+from AyraRobot import pbot
+from AyraRobot.utils.errors import capture_err
+from AyraRobot.utils.functions import make_carbon
 
 
 @pbot.on_message(filters.command("carbon"))
