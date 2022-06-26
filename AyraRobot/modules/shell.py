d1e175d8270a6b90bcd5f90c28df49c0362ec9bf
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 07:32:29 +0530

    Update shell.py

--- AyraRobot/modules/shell.py
+++ AyraRobot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from AyraRobot import LOGGER, dispatcher
+from AyraRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
