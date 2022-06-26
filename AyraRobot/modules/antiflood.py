50bdec0ea7bc7f6b29ec6710936790de9c78da3f
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 21:11:55 +0530

    Update antiflood.py

--- AyraRobot/modules/antiflood.py
+++ AyraRobot/modules/antiflood.py
@@ -4,15 +4,15 @@ import re
 
 from telegram import Message, Chat, Update, User, ChatPermissions
 
-from FallenRobot import TIGERS, WOLVES, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import (
+from AyraRobot import TIGERS, WOLVES, dispatcher
+from AyraRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql import antiflood_sql as sql
+from AyraRobot.modules.log_channel import loggable
+from AyraRobot.modules.sql import antiflood_sql as sql
 from telegram.error import BadRequest
 from telegram.ext import (
     CallbackContext,
@@ -23,10 +23,10 @@ from telegram.ext import (
     run_async,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.sql.approve_sql import is_approved
+from AyraRobot.modules.helper_funcs.string_handling import extract_time
+from AyraRobot.modules.connection import connected
+from AyraRobot.modules.helper_funcs.alternate import send_message
+from AyraRobot.modules.sql.approve_sql import is_approved
 
 FLOOD_GROUP = 3
 
