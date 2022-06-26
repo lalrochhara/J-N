6267bbad769f385371f8b958e109dbf9fb566d98
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 19:07:56 +0530

    Update currency_converter.py

--- AyraRobot/modules/currency_converter.py
+++ AyraRobot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from FallenRobot import CASH_API_KEY, dispatcher
+from AyraRobot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
