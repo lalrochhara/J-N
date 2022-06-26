b5c26ceee5da62839e272db96730ceca7ba44475
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 07:40:08 +0530

    Update remote_cmds.py

--- AyraRobot/modules/remote_cmds.py
+++ AyraRobot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from FallenRobot import dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import (
+from AyraRobot import dispatcher
+from AyraRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from FallenRobot.modules.helper_funcs.extraction import extract_user_and_text
-from FallenRobot.modules.helper_funcs.filters import CustomFilters
+from AyraRobot.modules.helper_funcs.extraction import extract_user_and_text
+from AyraRobot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
