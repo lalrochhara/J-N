bcb039f399b79ca372a9b427cca147341b909c70
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 06:16:09 +0530

    Update wallpaper.py

--- AyraRobot/modules/wallpaper.py
+++ AyraRobot/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from FallenRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from AyraRobot import SUPPORT_CHAT, WALL_API, dispatcher
+from AyraRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
