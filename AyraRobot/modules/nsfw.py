659c57761e192a49802154695b096613c135cf75
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 16:28:16 +0530

    Update nsfw.py

--- AyraRobot/modules/nsfw.py
+++ AyraRobot/modules/nsfw.py
@@ -4,14 +4,14 @@ import nekos
 import requests
 from PIL import Image
 from telegram import ParseMode
-from FallenRobot import dispatcher, updater
-import FallenRobot.modules.sql.nsfw_sql as sql
-from FallenRobot.modules.log_channel import gloggable
+from AyraRobot import dispatcher, updater
+import AyraRobot.modules.sql.nsfw_sql as sql
+from AyraRobot.modules.log_channel import gloggable
 from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CommandHandler, run_async, CallbackContext
-from FallenRobot.modules.helper_funcs.filters import CustomFilters
-from FallenRobot.modules.helper_funcs.chat_status import user_admin
+from AyraRobot.modules.helper_funcs.filters import CustomFilters
+from AyraRobot.modules.helper_funcs.chat_status import user_admin
 from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
 @run_async
