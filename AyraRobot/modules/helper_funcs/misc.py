0ffc16037942e313df40f60f9131f4b77f7f6399
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 21:24:22 +0530

    Update misc.py

--- AyraRobot/modules/helper_funcs/misc.py
+++ AyraRobot/modules/helper_funcs/misc.py
@@ -13,7 +13,7 @@ from telegram import (
 )
 from telegram.error import TelegramError
 
-from FallenRobot import NO_LOAD
+from AyraRobot import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
