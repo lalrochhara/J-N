66b777efd4b5b76384312e89149dc3b8b53466dc
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 19:03:51 +0530

    Update dbcleanup.py

--- AyraRobot/modules/dbcleanup.py
+++ AyraRobot/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import FallenRobot.modules.sql.global_bans_sql as gban_sql
-import FallenRobot.modules.sql.users_sql as user_sql
-from FallenRobot import DEV_USERS, OWNER_ID, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+import AyraRobot.modules.sql.global_bans_sql as gban_sql
+import AyraRobot.modules.sql.users_sql as user_sql
+from AyraRobot import DEV_USERS, OWNER_ID, dispatcher
+from AyraRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
