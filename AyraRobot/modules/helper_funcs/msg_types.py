c2a857f8e98aae2fd20aedc9e60d3cadd6c2a153
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 21:23:30 +0530

    Update msg_types.py

--- AyraRobot/modules/helper_funcs/msg_types.py
+++ AyraRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from FallenRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from AyraRobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
