ff701204fa2a2da621caf0ac8306827800402753
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 21:28:31 +0530

    Update chat_status.py

--- AyraRobot/modules/helper_funcs/chat_status.py
+++ AyraRobot/modules/helper_funcs/chat_status.py
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from FallenRobot.modules import connection
+from AyraRobot.modules import connection
 
 connected = connection.connected
