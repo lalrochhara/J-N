fcc2f9987b77c4cb341f0451a22193f0b7374b99
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 05:59:18 +0530

    Update wiki.py

--- AyraRobot/modules/wiki.py
+++ AyraRobot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from AyraRobot import dispatcher
+from AyraRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
