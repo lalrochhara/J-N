4d1898b3668ac44974dd86c062e3ca344bf98b6d
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 19:09:47 +0530

    Update country.py

--- AyraRobot/modules/country.py
+++ AyraRobot/modules/country.py
@@ -5,8 +5,8 @@ from telethon.utils import get_input_location
 import flag
 import html, os
 from countryinfo import CountryInfo
-from FallenRobot import telethn as borg
-from FallenRobot.events import register
+from AyraRobot import telethn as borg
+from AyraRobot.events import register
 
 
 @register(pattern="^/country (.*)")
