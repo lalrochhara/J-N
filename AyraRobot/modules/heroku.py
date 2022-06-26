440e9805e0945e5fdd7892a98fc84ae1b82b0492
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 18:38:25 +0530

    Update heroku.py

--- AyraRobot/modules/heroku.py
+++ AyraRobot/modules/heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from FallenRobot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from FallenRobot.events import register
+from AyraRobot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from AyraRobot.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
