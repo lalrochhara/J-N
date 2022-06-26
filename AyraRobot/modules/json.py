fcd18557fb3ae22299b3e19bb440a08ff0286234
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 18:36:08 +0530

    Update json.py

--- AyraRobot/modules/json.py
+++ AyraRobot/modules/json.py
@@ -1,6 +1,6 @@
 import io
-from FallenRobot.events import register
-from FallenRobot import telethn as tbot
+from AyraRobot.events import register
+from AyraRobot import telethn as tbot
 from telethon import types
 from telethon import events
 from telethon.tl import functions
