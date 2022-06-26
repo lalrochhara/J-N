1dc846a1a76084a4fd49225be8f4c16538edefc9
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 06:22:48 +0530

    Update users.py

--- AyraRobot/modules/users.py
+++ AyraRobot/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import FallenRobot.modules.sql.users_sql as sql
-from FallenRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from FallenRobot.modules.sql.users_sql import get_all_users
+import AyraRobot.modules.sql.users_sql as sql
+from AyraRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from AyraRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from AyraRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
