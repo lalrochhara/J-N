54709c3b3be184423adf38fa53fd0bbd04ee0fab
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 07:20:42 +0530

    Update speed_test.py

--- AyraRobot/modules/speed_test.py
+++ AyraRobot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from FallenRobot import DEV_USERS, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from AyraRobot import DEV_USERS, dispatcher
+from AyraRobot.modules.disable import DisableAbleCommandHandler
+from AyraRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
@@ -48,7 +48,7 @@ def speedtestxyz_callback(update: Update, context: CallbackContext):
             replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
             update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
     else:
-        query.answer("You are required to join @DevilsHeavenMF to use this command.")
+        query.answer("You are required to join @CrazyWorldChatting to use this command.")
 
 
 SPEED_TEST_HANDLER = DisableAbleCommandHandler("speedtest", speedtestxyz)
