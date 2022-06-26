b8efa03d8a138b073c502240d2e1deb869457cb8
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 21:10:11 +0530

    Update approve.py

--- AyraRobot/modules/approve.py
+++ AyraRobot/modules/approve.py
@@ -5,12 +5,12 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 from telegram.utils.helpers import mention_html
 
-import FallenRobot.modules.sql.approve_sql as sql
-from FallenRobot import DRAGONS, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import user_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_user
-from FallenRobot.modules.log_channel import loggable
+import AyraRobot.modules.sql.approve_sql as sql
+from AyraRobot import DRAGONS, dispatcher
+from AyraRobot.modules.disable import DisableAbleCommandHandler
+from AyraRobot.modules.helper_funcs.chat_status import user_admin
+from AyraRobot.modules.helper_funcs.extraction import extract_user
+from AyraRobot.modules.log_channel import loggable
 
 
 @loggable
