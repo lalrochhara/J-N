e7322659d6667a1b0733fb04be25421895d8745e
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 18:28:43 +0530

    Update locks.py

--- AyraRobot/modules/locks.py
+++ AyraRobot/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import FallenRobot.modules.sql.locks_sql as sql
-from FallenRobot import dispatcher, DRAGONS, LOGGER
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import (
+import AyraRobot.modules.sql.locks_sql as sql
+from AyraRobot import dispatcher, DRAGONS, LOGGER
+from AyraRobot.modules.disable import DisableAbleCommandHandler
+from AyraRobot.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.sql.approve_sql import is_approved
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
+from AyraRobot.modules.log_channel import loggable
+from AyraRobot.modules.connection import connected
+from AyraRobot.modules.sql.approve_sql import is_approved
+from AyraRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
