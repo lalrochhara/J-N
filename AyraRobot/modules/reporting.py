c1f2651a3f8fdf7210648c7d30727a1f1d7a7e08
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 07:37:19 +0530

    Update reporting.py

--- AyraRobot/modules/reporting.py
+++ AyraRobot/modules/reporting.py
@@ -1,9 +1,9 @@
 import html
 
-from FallenRobot import LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql import reporting_sql as sql
+from AyraRobot import LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher
+from AyraRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from AyraRobot.modules.log_channel import loggable
+from AyraRobot.modules.sql import reporting_sql as sql
 from telegram import Chat, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
