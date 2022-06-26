f9fca4590d08efae269ffafcd5a7b8015b720c7e
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 19:00:06 +0530

    Update dev.py

--- AyraRobot/modules/dev.py
+++ AyraRobot/modules/dev.py
@@ -7,8 +7,8 @@ from time import sleep
 
 import FallenRobot
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from AyraRobot import dispatcher
+from AyraRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
@@ -21,9 +21,9 @@ def allow_groups(update: Update, context: CallbackContext):
         update.effective_message.reply_text(f"Current state: {FallenRobot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
-        FallenRobot.ALLOW_CHATS = True
+        AyraRobot.ALLOW_CHATS = True
     elif args[0].lower() in ["yes", "on"]:
-        FallenRobot.ALLOW_CHATS = False
+        AyraRobot.ALLOW_CHATS = False
     else:
         update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
         return
