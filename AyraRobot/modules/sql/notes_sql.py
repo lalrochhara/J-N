3f016e61f1da8ed0efad6e12a98862eba4305f63
MISS-AYRA <108186595+MISS-AYRA@users.noreply.github.com>
2022-06-26 21:39:10 +0530

    Update notes_sql.py

--- AyraRobot/modules/sql/notes_sql.py
+++ AyraRobot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from FallenRobot.modules.helper_funcs.msg_types import Types
-from FallenRobot.modules.sql import BASE, SESSION
+from AyraRobot.modules.helper_funcs.msg_types import Types
+from AyraRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText, distinct, func
 
 
